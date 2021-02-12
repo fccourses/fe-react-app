db050a6f52d8a68dae3e1e8ac6947d2433104085
Vladimir <freshcode.yt@gmail.com>
2021-02-12 17:49:17 +0200

    Prepare project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import AlohaDashboard from './components/AlohaDashboard';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <AlohaDashboard />
-        
-      </>
-    );
-  }
+function App(props) {
+  return <> </>;
 }
 
 export default App;
