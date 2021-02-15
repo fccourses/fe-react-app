57908d5ad465591ddf7eb9699dc695bff085e15c
Vladimir <freshcode.yt@gmail.com>
2021-02-15 20:35:29 +0200

    Add classnames

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
 import styles from './LoginForm.module.css';
 
 const initialValues = {
   email: '',
   password: '',
+  isemailValid: true,
+  ispasswordValid: true,
 };
 
 class SignInForm extends Component {
@@ -12,33 +15,39 @@ class SignInForm extends Component {
     this.state = { ...initialValues };
   }
 
-  handleChange = ({ target: { name, value } }) =>
-    this.setState({ [name]: value });
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value, [`is${name}Valid`]: !value.includes(' ') });
+  };
 
   handleForm = (event) => {
     event.preventDefault();
     console.log(event);
 
     // event.target.reset();
-    this.setState({...initialValues});
+    this.setState({ ...initialValues });
   };
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, isemailValid } = this.state;
+
+    const emailClassNames = cx(styles.input, {
+      [styles.invalidInput]: !isemailValid,
+    });
+
     return (
       <form className={styles.container} onSubmit={this.handleForm}>
         <input
+          className={emailClassNames}
           onChange={this.handleChange}
           value={email}
-          className={styles.input}
           placeholder='Email'
           type='email'
           name='email'
         />
         <input
+          className={styles.input}
           onChange={this.handleChange}
           value={password}
-          className={styles.input}
           placeholder='Password'
           type='password'
           name='password'
@@ -50,3 +59,26 @@ class SignInForm extends Component {
 }
 
 export default SignInForm;
+
+/* function cx(objectStyles) {
+  return Object.entries(objectStyles)
+    .filter(([className, condition]) => condition)
+    .map(([className, condition]) => className)
+    .join(' ');
+} */
+
+/* 
+
+{
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+  имяКласса: условиеДляПрименения,
+}
+*/
