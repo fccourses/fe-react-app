2927a643bc9f486766377aaf60b5dc95d34ff82b
Vladimir <freshcode.yt@gmail.com>
2021-02-15 20:12:11 +0200

    Add controlled input

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import styles from './LoginForm.module.css';
 
+const initialValues = {
+  email: '',
+  password: '',
+};
+
 class SignInForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
+    this.state = { ...initialValues };
   }
+
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({ [name]: value });
+
   handleForm = (event) => {
     event.preventDefault();
     console.log(event);
 
-    event.target.reset();
+    // event.target.reset();
+    this.setState({...initialValues});
   };
+
   render() {
     const { email, password } = this.state;
     return (
       <form className={styles.container} onSubmit={this.handleForm}>
         <input
-          // onChange={} <- TASK. Брать новое значение и устанавливать в состояние
+          onChange={this.handleChange}
           value={email}
           className={styles.input}
           placeholder='Email'
@@ -28,6 +36,7 @@ class SignInForm extends Component {
           name='email'
         />
         <input
+          onChange={this.handleChange}
           value={password}
           className={styles.input}
           placeholder='Password'
