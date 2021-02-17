a1bb0f59f942c587a81a163f4d9511a68183e0a8
Vladimir <freshcode.yt@gmail.com>
2021-02-17 19:02:15 +0200

    Fix DOM nesting

--- src/components/Calendar/Month/index.jsx
+++ src/components/Calendar/Month/index.jsx
@@ -4,15 +4,13 @@ import Week from '../Week';
 const Month = props => {
   const { year, month } = props;
 
-  
-
   return (
-    <div>
+    <>
       <Week year={2021} week={2} />
       <Week year={2021} week={3} />
       <Week year={2021} week={4} />
       <Week year={2021} week={5} />
-    </div>
+    </>
   );
 };
 
