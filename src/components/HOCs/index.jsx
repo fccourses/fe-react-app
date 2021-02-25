fd9919b0ba2ee763bede64aef5f343254745586d
Vladimir <freshcode.yt@gmail.com>
2021-02-25 20:43:58 +0200

    Fix HOCs

--- src/components/HOCs/index.jsx
+++ src/components/HOCs/index.jsx
@@ -1,13 +1,40 @@
-import { ThemeContext } from '../../contexts';
+import { Component } from 'react';
+import { ThemeContext, UserContext } from '../../contexts';
 
 export const withTheme = WrappedComponent => {
-  return props => {
+  function WrappedComponentWithTheme (props) {
     return (
       <ThemeContext.Consumer>
         {([theme, setTheme]) => {
-          return <WrappedComponent theme={theme} setTheme={setTheme} />;
+          return (
+            <WrappedComponent theme={theme} setTheme={setTheme} {...props} />
+          );
         }}
       </ThemeContext.Consumer>
     );
-  };
+  }
+
+  return WrappedComponentWithTheme;
+};
+
+export const withUser = WrappedComponent => {
+  class WrappedComponentWithUser extends Component {
+    /*     
+    constructor (props) {
+      super(props);
+      this.state = {};
+    }
+
+    componentDidMount () {}
+    componentDidUpdate (prevProps, prevState) {} */
+    render () {
+      return (
+        <UserContext.Consumer>
+          {user => <WrappedComponent user={user} {...this.props} />}
+        </UserContext.Consumer>
+      );
+    }
+  }
+
+  return WrappedComponentWithUser;
 };
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
 import { Brightness3, WbSunny } from '@material-ui/icons';
-import { UserContext, ThemeContext } from '../../contexts';
-import { withTheme } from '../HOCs';
+import { withTheme, withUser } from '../HOCs';
 import styles from './Header.module.scss';
 import CONSTANTS from '../../constants';
 
@@ -10,39 +9,33 @@ const { THEMES } = CONSTANTS;
 
 class Header extends Component {
   render () {
-    const { theme, setTheme } = this.props;
-
+    const { theme, setTheme, user } = this.props;
     const isDarkTheme = theme === THEMES.DARK;
-
     const classNames = cx(styles.container, {
       [styles.lightTheme]: theme === THEMES.LIGHT,
       [styles.darkTheme]: isDarkTheme,
     });
 
     return (
-      <UserContext.Consumer>
-        {user => (
-          <div className={classNames}>
-            <span>
-              {user.firstName} {user.lastName}
-            </span>
-            <img src={user.imageSrc} alt='doe' />
+      <div className={classNames}>
+        <span>
+          {user.firstName} {user.lastName}
+        </span>
+        <img src={user.imageSrc} alt='doe' />
 
-            <div
-              onClick={() => {
-                const nextTheme = isDarkTheme ? THEMES.LIGHT : THEMES.DARK;
-                setTheme(nextTheme);
-              }}
-            >
-              {isDarkTheme ? <Brightness3 /> : <WbSunny />}
-            </div>
-          </div>
-        )}
-      </UserContext.Consumer>
+        <div
+          onClick={() => {
+            const nextTheme = isDarkTheme ? THEMES.LIGHT : THEMES.DARK;
+            setTheme(nextTheme);
+          }}
+        >
+          {isDarkTheme ? <Brightness3 /> : <WbSunny />}
+        </div>
+      </div>
     );
   }
 }
 
 const HeaderWithTheme = withTheme(Header);
 
-export default HeaderWithTheme;
+export default withUser(HeaderWithTheme);
