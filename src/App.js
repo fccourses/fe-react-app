d290059faac12e1f8ded536a81ff03887541a29d
Vladimir <freshcode.yt@gmail.com>
2021-03-03 20:46:26 +0200

    Add task example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import { UserContext } from './contexts';
 
 const App = props => {
+  const [user, setUser] = useState({
+    id: 1,
+    name: 'John Doe',
+  });
+
+  /*>> ThemeSwitcher <<*/
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <UserContext.Provider value={user}>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import StopWatch from '../../components/FuncStopWatch';
 import Clicker from '../../components/FuncTracker';
+import { UserContext } from '../../contexts';
 
 const Home = props => {
   const [isVisible, setIsVisible] = useState(true);
 
+  const value = useContext(UserContext);
+
   return (
     <div>
       <button onClick={() => setIsVisible(!isVisible)}>SWITCH</button>
+      <p>{JSON.stringify(value)}</p>
       {/* {isVisible && <Clicker />} */}
-      <StopWatch />
+      {/* <StopWatch /> */}
     </div>
   );
 };
